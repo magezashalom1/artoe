ea6f9d9ae6e32cf647e41a420f381d20a0762532
Shalom Mageza <97250002+magezashalom1@users.noreply.github.com>
2024-10-25 20:13:45 +0000

    adding frrontend to render

--- artoe/urls.py
+++ artoe/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] 
+    # Serve the React app
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html"), name='home'),
+]
+
+# Serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+    
