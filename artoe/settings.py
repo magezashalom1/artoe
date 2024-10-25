79408e5680c331bcb09bdfc6ac03744870a2f7fb
Shalom Mageza <97250002+magezashalom1@users.noreply.github.com>
2024-10-25 16:20:38 +0000

    New Front End React App

--- artoe/settings.py
+++ artoe/settings.py
@@ -110,3 +110,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media files (for file uploads, if applicable)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
--- artoe/static/readme.md
+++
@@ -1,1 +0,0 @@
-The static files Here
