9e0fae5c7b7146320559079635d04af618271f2a
Shalom Mageza <97250002+magezashalom1@users.noreply.github.com>
2024-10-25 00:38:44 +0000

    Added dummy sneakers to the database

--- products/urls.py
+++ products/urls.py
@@ -1,19 +1,8 @@
 
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
-from . import views
-from django.contrib import admin
-from django.urls import path, include
-
+from .views import SneakerListView, sneaker_detail
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    ]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    path('', SneakerListView.as_view(), name='sneaker-list'),
+    path('<int:pk>/', sneaker_detail, name='sneaker-detail'),  # Detail view
+]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404
 from .models import Sneaker
+from django.views.generic import ListView
 
-def sneaker_list(request):
-    sneakers = Sneaker.objects.all()
-    return render(request, 'products/sneaker_list.html', {'sneakers': sneakers})
+# Class-based view for listing sneakers
+class SneakerListView(ListView):
+    model = Sneaker
+    template_name = 'products/sneaker_list.html'  # Corrected path
+    context_object_name = 'sneakers'  # This will be the variable in the template
 
+# Function-based view for sneaker details
 def sneaker_detail(request, pk):
     sneaker = get_object_or_404(Sneaker, pk=pk)
     return render(request, 'products/sneaker_detail.html', {'sneaker': sneaker})
