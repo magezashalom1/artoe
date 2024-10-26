caa0f02f3104e4f36392cfdf0bd4566e2e6a5a73
Shalom Mageza <97250002+magezashalom1@users.noreply.github.com>
2024-10-26 23:51:35 +0000

    moved build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage'; // Update the import path to match the new structure
-import SneakerList from './components/SneakerList'; // Import the SneakerList component
-import Products from './pages/Products'; // Import the Products page
-import About from './pages/About'; // Import the About page
-import FAQs from './pages/FAQs'; // Import the FAQs page
-import Contact from './pages/Contact'; // Import the Contact page
+import HomePage from './pages/HomePage'; // Correct import path for HomePage
+import SneakerList from './components/SneakerList'; // Import SneakerList component
+import Products from './pages/Products'; // Import Products page
+import AboutUs from './pages/AboutUs'; // Correct import path for AboutUs page
+import FAQs from './pages/FAQs'; // Import FAQs page
+import ContactUs from './pages/ContactUs'; // Correct import path for ContactUs page
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         <Route path="/" exact component={HomePage} /> {/* Render HomePage on the root path */}
         <Route path="/sneakers" component={SneakerList} /> {/* Route for SneakerList */}
         <Route path="/products" component={Products} /> {/* Route for Products */}
-        <Route path="/about" component={About} /> {/* Route for About */}
+        <Route path="/about" component={AboutUs} /> {/* Route for AboutUs */}
         <Route path="/faqs" component={FAQs} /> {/* Route for FAQs */}
-        <Route path="/contact" component={Contact} /> {/* Route for Contact */}
+        <Route path="/contact" component={ContactUs} /> {/* Route for ContactUs */}
         {/* Add other routes as needed */}
       </Switch>
     </Router>
@@ -24,6 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-
